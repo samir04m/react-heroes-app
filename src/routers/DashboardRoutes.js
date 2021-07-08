c2f1d0e2af50678eec562f196bb0d24fcf26fb25
Samir Mejia <samirmejiaat@unimagdalena.edu.co>
2021-07-08 15:46:48 -0500

    List superheroes by publisher

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -11,7 +11,7 @@ export const DashboardRoutes = () => {
         <>
             <Navbar/>
 
-            <div>
+            <div className="container mt-2">
                 <Switch>
                     <Route exact path="/marvel" component={ MarvelScreen  } />
 
