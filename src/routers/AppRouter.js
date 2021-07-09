0729a0b87290775adf23956de3e2bda4c96ead7b
Samir Mejia <samirmejiaat@unimagdalena.edu.co>
2021-07-08 23:15:23 -0500

    Public Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
-    Switch,
-    Route
+    Switch
 } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute ';
 
 export const AppRouter = () => {
 
@@ -18,7 +18,7 @@ export const AppRouter = () => {
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />
+                    <PublicRoute exact path="/login" component={ LoginScreen } isAuthenticated={ user.logged } />
  
                     <PrivateRoute path="/" component={ DashboardRoutes } isAuthenticated={ user.logged } />
                 
